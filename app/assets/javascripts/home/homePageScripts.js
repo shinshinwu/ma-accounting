953622fd5ae58646cd57e3850a61f5475822a22f
Marlon J. Frausto <marlonjfrausto@gmail.com>
2017-03-14 22:27:15 -0700

    basic markup completed for home page.

--- app/assets/javascripts/home/homePageScripts.js
+++ app/assets/javascripts/home/homePageScripts.js
@@ -10,6 +10,20 @@ $(document).on('page:load', ready);
 
 
 function initializeMaterializeElements(){
+  
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.modal').modal();
+
+    options = [
+      {selector: '#image-test', offset: 0, callback: function(el) {
+        Materialize.fadeInImage($(el));
+      } }
+    ];
+    Materialize.scrollFire(options);
+
+    $('.collapsible').collapsible();
+    
   console.log("initialized materialized elements");
 }
 
