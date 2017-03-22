d135c8702b6ec07ddb8a33de12150b04edaed697
Anna Wu <shinshinwu@gmail.com>
2017-03-22 14:44:31 -0700

    adding reminder email template

--- app/mailers/member_mailer.rb
+++ app/mailers/member_mailer.rb
@@ -18,6 +18,21 @@ class MemberMailer < ActionMailer::Base
     end
   end
 
+  def discount_reminder(member_id, promotion_id:, campaign_id:)
+    data = {campaign_id: campaign_id}
+    @member = Member.find_by_id(member_id)
+    @launch_date = Date.new(2017, 04, 30)
+
+    if @member.present?
+      @promo = Promotion.find_by_id(promotion_id)
+      @tracking_info = Base64.urlsafe_encode64({promotion_id: promotion_id, email: @member.email}.to_json)
+      subject = "Hi #{@member.nickname}, I’d like to refer you some new clients"
+      mail(from: "Marlon from Modern Assets <#{Settings.support_email}>",to: @member.email, subject: subject, sparkpost_data: data) do |format|
+        format.html { render layout: false }
+      end
+    end
+  end
+
   def initial_launch(member_id)
     data = {campaign_id: "acounting-inital-launch"}
     @member = Member.find_by_id(member_id)
