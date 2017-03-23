3d8a585acda58e0cb057cae37de3b84cdb82d527
Anna Wu <shinshinwu@gmail.com>
2017-03-23 14:50:26 -0700

    remove logging dev errors from sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,6 @@
 # set up some configuration to be used for error logging aka Raven Sentry
 Raven.configure do |config|
   config.dsn = ENV['SENTRY_DSN']
-  config.environments = %w[development production]
+  config.environments = %w[production]
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
 end
